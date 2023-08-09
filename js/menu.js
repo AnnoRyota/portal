32a4c00fd0d170b509e9e54b20fda86d8c3a0020
Anno Ryota <anno1216.rad@gmail.com>
2023-08-09 14:34:44 +0900

    menu design fiz

--- js/menu.js
+++ js/menu.js
@@ -1,65 +1,17 @@
-(function() {
-  let menuSwitch = document.querySelector("#menu-switch"),
-    sidebar = document.querySelector("#sidebar"),
-    main = document.querySelector("#main");
-
-  function toggleSidebar() {
-    let classesToApplyForSidebar = {
-        active: [],
-        inactive: ["sidebar-inactive"]
-      },
-      classesToApplyForMain = {
-        active: ["md:pl-[224px]"],
-        inactive: []
-      },
-      classesToApplyForMenuButton = {
-        active: ["fa-caret-left"],
-        inactive: ["fa-bars"]
-      };
-
-    let isMenuActive = menuSwitch.getAttribute("data-menu-active") === "true";
-
-    if (isMenuActive) {
-      menuSwitch.setAttribute("data-menu-active", null);
-
-      menuSwitch.children[0].classList.remove(
-        ...classesToApplyForMenuButton.active
-      );
-      menuSwitch.children[0].classList.add(
-        ...classesToApplyForMenuButton.inactive
-      );
-
-      sidebar.classList.remove(...classesToApplyForSidebar.active);
-      sidebar.classList.add(...classesToApplyForSidebar.inactive);
-
-      main.classList.remove(...classesToApplyForMain.active);
-      main.classList.add(...classesToApplyForMain.inactive);
-    } else {
-      menuSwitch.setAttribute("data-menu-active", true);
-
-      menuSwitch.children[0].classList.add(
-        ...classesToApplyForMenuButton.active
-      );
-      menuSwitch.children[0].classList.remove(
-        ...classesToApplyForMenuButton.inactive
-      );
-
-      sidebar.classList.add(...classesToApplyForSidebar.active);
-      sidebar.classList.remove(...classesToApplyForSidebar.inactive);
-
-      main.classList.add(...classesToApplyForMain.active);
-      main.classList.remove(...classesToApplyForMain.inactive);
-    }
-  }
-
-  document.querySelector("#menu-switch").addEventListener("click", e => {
-    e.preventdefault;
-    toggleSidebar();
+document.querySelector(".nav-toggle").addEventListener("click", function () {
+  this.classList.toggle('active');
+  document.querySelector("#nav").classList.toggle('panelactive');
+  document.querySelectorAll(".circle-bg").forEach(function(element) {
+    element.classList.toggle('circleactive');
   });
+});
 
-  toggleSidebar();
-  setTimeout(() => {
-    sidebar.classList.add("slow-moves");
-    main.classList.add("slow-moves");
-  }, 300);
-})();
+document.querySelectorAll("#nav a").forEach(function(element) {
+  element.addEventListener("click", function () {
+    document.querySelector(".nav-toggle").classList.remove('active');
+    document.querySelector("#nav").classList.remove('panelactive');
+    document.querySelectorAll(".circle-bg").forEach(function(element) {
+      element.classList.remove('circleactive');
+    });
+  });
+});
