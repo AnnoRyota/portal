1e31c76395b09431c029c5dc519abd12042992a0
Anno Ryota <anno1216.rad@gmail.com>
2023-08-29 11:16:48 +0900

    contact form

---
+++ js/mail.js
@@ -0,0 +1,27 @@
+// ローカル環境
+//const domain = 'http://localhost';
+// ステージング環境
+const domain = 'https://pf.staging.kindai.design';
+// 本番環境
+//const domain = 'https://pf.kindai.design';
+// ポータル
+//const domain = 'https://main.d2vr2ta1g7th0h.amplifyapp.com'
+
+const sendMailUrl = `${domain}/api/contact/company`;
+
+function sendMail() {
+    const form = document.getElementById('mail_form');
+    const formData = new FormData(form);
+
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', sendMailUrl);
+    xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
+    xhr.send(JSON.stringify(data));
+}
+
+document.getElementById('mail_send').onclick = function(){
+    sendMail();
+};
