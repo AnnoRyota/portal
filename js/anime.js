d89058150516b0039c6f41227137051945f9d3df
Anno Ryota <anno1216.rad@gmail.com>
2023-08-31 13:21:12 +0900

    pre

--- js/anime.js
+++ js/anime.js
@@ -10,4 +10,18 @@ $(window).scroll(function (){
         $(this).removeClass("show");
       }
     });
+});
+
+$(window).scroll(function (){
+    $(".video").each(function(){
+      var offset    = $(this).offset().top;
+      var scroll    = $(window).scrollTop();
+      var wHeight   = $(window).height();
+
+      if (scroll > offset - wHeight + wHeight/2){
+        $(this).addClass("show");
+      } else {
+        $(this).removeClass("show");
+      }
+    });
   });
