4d07934a6727f2d6539816dd16b35b9cbf3d8b5b
Anno Ryota <anno1216.rad@gmail.com>
2025-08-25 18:54:08 +0900

    protofes 03

--- js/cssanimation.js
+++ js/cssanimation.js
@@ -92,6 +92,62 @@ function animateScroll() {
   });
 }
 
+function animateScroll() {
+  const posters = document.querySelectorAll('.poster_03');
+
+  posters.forEach((poster, index) => {
+    const entryStart = index * 100;
+    const entryEnd = (index + 1 * 1000);
+    const exitStart = entryStart + 100;
+    const exitEnd = exitStart + 100;
+
+    // 対象の要素に no-entry-animation クラスが含まれている場合は exit のアニメーションのみを実行
+    if (poster.classList.contains('no-entry-animation')) {
+      poster.animate(
+        {
+          opacity: [1, 0],
+          transform: ['scale(1)', 'scale(1.5)'],
+        },
+        {
+          fill: 'both',
+          timeline: new ViewTimeline({ subject: poster }),
+          rangeStart: { rangeName: 'exit', offset: CSS.percent(0) },
+          rangeEnd: { rangeName: 'exit', offset: CSS.percent(100) },
+        }
+      );
+    } else {
+      // 通常のアニメーションを適用
+      poster.animate(
+        {
+          // opacity: [1 , 1, .7,],
+          transform: [ 'scale(1)', 'scale(.7)'],
+        },
+        {
+          fill: 'both',
+          timeline: new ViewTimeline({ subject: poster }),
+          rangeStart: [
+            { rangeName: 'entry', offset: CSS.percent(0) },
+            { rangeName: 'exit', offset: CSS.percent(exitStart) },
+          ],
+          rangeEnd: [
+            { rangeName: 'entry', offset: CSS.percent(entryEnd) },
+            { rangeName: 'exit', offset: CSS.percent(exitEnd) },
+          ],
+        }
+      );
+
+      // ビューポート内にあるときはアニメーション開始
+      let animationStarted = false;
+      poster.addEventListener('animationstart', () => {
+        if (isElementInViewport(poster) && !animationStarted) {
+          poster.style.transform = 'scale(1)';
+          animationStarted = true;
+        }
+      });
+    }
+  });
+}
+
 
         // 初期化関数
         function init() {
